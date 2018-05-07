5fa9f60d02adfa833f635025fe81e0617b77be81
reenavermaiiitm <reenavermaiiitm@gmail.com>
2018-05-08 00:46:56 +0530

    final commit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,42 +1,48 @@
-import React,{Component }from 'react';
-import {connect} from 'react-redux';
-import {Sparklines,SparklinesLine} from 'react-sparkline';
-class WeatherList extends Component{
-  renderWeather(cityData){
-    const name=cityData.city.name
-    const temps=cityData.list.map(weather=>weather.main.temp)
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
 
-    return(
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+
+    return (
       <tr key={name}>
-         <td>{name}</td>
-         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red"/>
-          </Sparklines>
-         </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
-  render(){
-    return(
+
+  render() {
+    return (
       <table className="table table-hover">
-       <thead>
-         <tr>
-          <th>City</th>
-           <th>Temprature</th>
-           <th>Pressure</th>
-           <th>Humidity</th>
-         </tr>
-       </thead>
-       <tbody>
-            {this.props.weather.map(this.renderWeather)}
-       </tbody>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+
+        </tbody>
       </table>
     );
   }
 }
-function mapStateToProps(state){
 
-  return {weather:state.weather};
+function mapStateToProps({ weather }) {
+  return { weather };
 }
+
 export default connect(mapStateToProps)(WeatherList);
